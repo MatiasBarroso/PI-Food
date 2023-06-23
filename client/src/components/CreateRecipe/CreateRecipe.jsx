01b2b6dc9bd9bb8af282856066c14ef00c2b6927
Matias Barroso <104941165+MatiasBarroso@users.noreply.github.com>
2023-06-23 15:50:13 -0300

    ui-form-updated

--- client/src/components/CreateRecipe/CreateRecipe.jsx
+++ client/src/components/CreateRecipe/CreateRecipe.jsx
@@ -6,6 +6,7 @@ import { useHistory } from 'react-router-dom';
 import { nameConverter } from './CreateRecipe.modules';
 import Footer from '../Footer/Footer'
 import Nav from '../Nav/Nav';
+import NavMobile from '../NavMobile';
 
 const CreateRecipe = () => {
 
@@ -20,6 +21,7 @@ const CreateRecipe = () => {
 
   ///           LOCAL STATES           ///
 
+  const [screenWidth, setScreenWidth] = useState(0);
   const [data, setData] = useState({
     name: "",
     summary: "",
@@ -177,6 +179,7 @@ const CreateRecipe = () => {
   }
 
 ///////////////////////////////////////////
+ 
 
   useEffect(() => {
     if(errors.name === "" && errors.summary === "" && errors.healthScore === "" && errors.image === "" ) setActive(false)
@@ -212,6 +215,13 @@ const CreateRecipe = () => {
     }
   },[dispatch, data])
 
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -301,17 +311,22 @@ const CreateRecipe = () => {
       healthScore: "Score must be between 1 and 100"
     })
   }
-  
 
   const addStepClick = () => {
    
     setInputs([...inputs, { value: '', num: inputs.length + 1 }]);
   }
 
+  const handleScroll = () => {
+    setScreenWidth(window.innerWidth);
+  };
+
+
+
   return (
     <div className='container-creater'>
       <div className='cont-nav-cr'>
-        <Nav />
+        {screenWidth > 430 ? <Nav /> : <NavMobile />}
       </div>
       <div className='cont-form'>
         <form className='container-form' onSubmit={handleSubmit}>
@@ -366,12 +381,12 @@ const CreateRecipe = () => {
             </div>
           </div>
         </form>
-        <button className='back-btn' onClick={goBackHandleClick} type='button'>
+        {/* <button className='back-btn' onClick={goBackHandleClick} type='button'>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
           </svg>
           Back
-        </button>
+        </button> */}
       </div>
       <Footer />
     </div>
