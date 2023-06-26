70db55d2c21e98fcbc35c4f1d94e79d7d8bb322f
Matias Barroso <matibarroso10@gmail.com>
2023-06-26 10:44:09 -0300

    pi ui uploaded

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Loading.css'
+
 const Loading = () => {
+
   return (
     <div>
+      <div>
         <h3 className='loading-text'>LOADING...</h3>
         <div className="circle"></div>
+      </div>
     </div>
   )
 }
